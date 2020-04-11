50ed9db437262bfb4be7004ea1165bd869aa69df
Vagrant Default User <vagrant@stretch.localdomain>
2020-04-11 09:52:15 +0000

    [add] first commit. get hello api.

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,64 @@
+
+const config = {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/proxy',
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  },
+
+  generate: {
+    dir: '../public'
+  }
+}
+
+if(process.env.NODE_ENV === 'development'){
+  config.proxy = { '/api' : 'http://192.168.2.2:3000'}
+}
+
+export default config
